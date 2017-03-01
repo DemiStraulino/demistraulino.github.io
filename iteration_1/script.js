4c2f0c117169fb9159197887d8521d195ceb0dc3
Demi Straulino <strad975@newschool.edu>
2017-03-01 14:27:36 -0500

    teal colors and black mouseover

--- iteration_1/script.js
+++ iteration_1/script.js
@@ -1,6 +1,6 @@
 $(document). ready(function() {
 	//apend new element
-	for (i = 0; i < 100; i++) { // increased # of boxes from '< 100' to '< 1000' 
+	for (i = 0; i < 500; i++) { // increased # of boxes from '< 100' to '< 1000' 
 		$("body").append( "<div class='element' style= 'background-color: hsl (" + i * 3.6 + ", 100%, 50%);'></div>"// hue(0-360), saturation, lightness ></div>"
 			);
 			console.log("element " + i + "added");// used to see the loop executed in 'console' 
