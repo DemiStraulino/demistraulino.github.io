acf249f929c15bd5fbb74420620ad56bf95df355
Demi Straulino <strad975@newschool.edu>
2017-05-12 06:13:55 -0400

    loading autocorrect

---
+++ autocorrect/assets/js/app.js
@@ -0,0 +1,127 @@
+var Awords = ["a", "able", "about", "above", "across", "act", "action", "actually", "add", "addition", "adjective", "afraid", "Africa", "after", "again", "against", "age", "ago", "agreed", "ahead", "air", "all", "allow", "almost", "alone", "along", "already", "also", "although", "always", "am", "America", "among", "amount", "an", "and", "angle", "animal", "another", "answer", "any", "anything", "appear", "apple", "are", "area", "arms", "army", "around", "arrived", "art", "as", "ask", "at", "away"];
+
+var Bwords = ["baby", "back", "bad", "ball", "bank", "base", "be", "bear", "beat", "beautiful", "became", "because", "become", "bed", "been", "before", "began", "begin", "behind", "being", "believe", "bell", "belong", "below", "beside", "best", "better", "between", "big", "bill", "birds", "bit", "black", "block", "blood", "blow", "blue", "board", "boat", "body", "bones", "book", "born", "both", "bottom", "bought", "box", "boy", "branches", "break", "bright", "bring", "British", "broken", "brother", "brought", "brown", "build", "building", "built", "burning", "business", "but", "buy", "by"];
+
+var Cwords = ["called", "came", "can", "cannot", "can’t", "capital", "captain", "car", "care", "carefully", "carry", "case", "cat", "catch", "cattle", "caught", "cause", "cells", "center", "cents", "century", "certain", "chance", "change", "chart", "check", "chief", "child", "children", "choose", "church", "circle", "city", "class", "clean", "clear", "climbed", "close", "clothes", "cloud", "coast", "cold", "color", "column", "come", "common", "company", "compare", "complete", "compound", "conditions", "consider", "consonant", "contain", "continued", "control", "cook", "cool", "copy", "corn", "corner", "correct", "cost", "cotton", "could", "couldn’t", "count", "country", "course", "covered", "cows", "create", "cried", "crops", "cross", "crowd", "current", "cut"];
+
+var Dwords = ["dance", "dark", "day", "dead", "deal", "death", "decided", "decimal", "deep", "describe", "desert", "design", "details", "determine", "developed", "dictionary", "did", "didn’t", "died", "difference", "different", "difficult", "direct", "direction", "discovered", "distance", "divided", "division", "do", "doctor", "does", "dog", "dollars", "done", "don’t", "door", "down", "draw", "drawing", "dress", "drive", "drop", "dry", "during"];
+
+var Ewords = ["each", "early", "ears", "earth", "east", "easy", "eat", "edge", "effect", "eggs", "eight", "either", "electric", "elements", "else", "end", "energy", "engine", "England", "English", "enjoy", "enough", "entered", "entire", "equal", "equation", "especially", "Europe", "even", "evening", "ever", "every", "everyone", "everything", "exactly", "example", "except", "exciting", "exercise", "expect", "experience", "experiment", "explain", "express", "eyes"];
+
+var Fwords = ["face", "fact", "factories", "factors", "fair", "fall", "family", "famous", "far", "farm", "farmers", "fast", "father", "fear", "feel", "feeling", "feet", "fell", "felt", "few", "field", "fig", "fight", "figure", "filled", "finally", "find", "fine", "fingers", "finished", "fire", "first", "fish", "fit", "five", "flat", "floor", "flow", "flowers", "fly", "follow", "food", "foot", "for", "force", "forest", "form", "forward", "found", "four", "fraction", "France", "free", "French", "fresh", "friends", "from", "front", "fruit", "full", "fun"];
+
+var Gwords = ["game", "garden", "gas", "gave", "general", "get", "girl", "give", "glass", "go", "God", "gold", "gone", "good", "got", "government", "grass", "great", "Greek", "green", "grew", "ground", "group", "grow", "guess", "gun"];
+
+var Hwords = ["had", "hair", "half", "hand", "happened", "happy", "hard", "has", "hat", "have", "he", "head", "hear", "heard", "heart", "heat", "heavy", "held", "help", "her", "here", "high", "hill", "him", "himself", "his", "history", "hit", "hoe", "hold", "hole", "home", "hope", "horse", "hot", "hours", "house", "how", "however", "huge", "human", "hundred", "hunting"];
+
+var Iwords = ["I", "ice", "idea", "if", "important", "in", "inches", "include", "increase", "Indian", "indicate", "industry", "information", "insects", "inside", "instead", "instruments", "interest", "interesting", "into", "iron", "is", "island", "isn’t", "it", "itself", "it’s", "I’ll"];
+
+var Jwords = ["Japanese", "job", "joined", "jumped", "just"];
+
+var Kwords = ["keep", "kept", "key", "killed", "kind", "king", "knew", "know", "known"];
+
+var Lwords = ["lady", "lake", "land", "language", "large", "last", "late", "laughed", "law", "lay", "lead", "learn", "least", "leave", "led", "left", "legs", "length", "less", "let", "letter", "let’s", "level", "lie", "life", "lifted", "light", "like", "line", "list", "listen", "little", "live", "located", "long", "look", "lost", "lot", "loud", "love", "low"];
+
+var Mwords = ["machine", "made", "main", "major", "make", "mall", "man", "many", "map", "march", "mark", "match", "material", "matter", "may", "maybe", "me", "means", "measure", "meat", "meet", "melody", "members", "men", "metal", "method", "middle", "might", "mile", "milk", "million", "mind", "mine", "minutes", "miss", "modern", "molecules", "moment", "money", "months", "moon", "more", "morning", "most", "mother", "mountains", "mouth", "move", "movement", "much", "music", "must", "my"];
+
+var Nwords = ["name", "nation", "natural", "near", "necessary", "need", "never", "new", "next", "night", "no", "north", "northern", "nose", "not", "note", "nothing", "notice", "noun", "now", "number", "numeral"];
+
+var Owords = ["object", "observe", "ocean", "of", "off", "office", "often", "oh", "oil", "old", "on", "once", "one", "only", "open", "opposite", "or", "order", "other", "our", "out", "outside", "over", "own", "oxygen"];
+
+var Pwords = ["page", "paint", "pair", "paper", "paragraph", "park", "part", "particular", "party", "passed", "past", "pattern", "pay", "people", "per", "perhaps", "period", "person", "phrase", "picked", "picture", "piece", "place", "plains", "plan", "plane", "planets", "plant", "play", "please", "plural", "poem", "point", "pole", "poor", "position", "possible", "pounds", "power", "practice", "prepared", "present", "president", "pretty", "printed", "probably", "problem", "process", "produce", "products", "property", "provide", "pulled", "pushed", "put"];
+
+var Qwords = ["questions", "quickly", "quiet", "quite"];
+
+var Rwords = ["race", "radio", "rain", "raised", "ran", "rather", "reached", "read", "ready", "real", "reason", "received", "record", "red", "region", "remain", "remember", "repeated", "report", "represent", "rest", "result", "return", "rhythm", "rich", "ride", "right", "ring", "rise", "river", "road", "rock", "rolled", "room", "root", "rope", "rose", "round", "row", "rule", "run"];
+
+var Swords = ["safe", "said", "sail", "same", "sand", "sat", "save", "saw", "say", "scale", "school", "science", "scientists", "score", "sea", "seat", "second", "section", "see", "seeds", "seem", "seen", "sell", "send", "sense", "sent", "sentence", "separate", "serve", "set", "settled", "seven", "several", "shall", "shape", "sharp", "she", "ship", "shoes", "shop", "short", "should", "shoulder", "shouted", "show", "shown", "side", "sight", "sign", "silent", "similar", "simple", "since", "sing", "single", "sir", "sister", "sit", "site", "six", "size", "skin", "sky", "sleep", "slowly", "small", "smell", "smiled", "snow", "so", "soft", "soil", "soldiers", "solution", "solve", "some", "someone", "something", "sometimes", "son", "song", "soon", "sound", "south", "southern", "space", "speak", "special", "speed", "spell", "spot", "spread", "spring", "square", "stand", "stars", "start", "state", "statement", "stay", "steel", "step", "stick", "still", "stone", "stood", "stop", "store", "story", "straight", "strange", "stream", "street", "stretched", "string", "strong", "students", "study", "subject", "substances", "such", "suddenly", "suffix", "sugar", "suggested", "sum", "summer", "sun", "supply", "suppose", "sure", "surface", "surprise", "swim", "syllables", "symbols", "system"];
+
+var Twords = ["table", "take", "talk", "tall", "teacher", "team", "tell", "temperature", "ten", "terms", "test", "than", "that","the", "their", "them", "themselves", "then", "there", "these", "they", "thick", "thin", "things", "think", "third", "this", "those", "though", "thought", "thousands", "three", "through", "thus", "tied", "time", "tiny", "to", "today", "together", "told", "tone", "too", "took", "tools", "top", "total", "touch", "toward", "town", "track", "trade", "train", "travel", "tree", "triangle", "trip", "trouble", "truck", "true", "try", "tube", "turn", "two", "type"];
+
+var Uwords = ["uncle", "under", "underline", "understand", "unit", "until", "up", "upon", "us", "use", "usually"];
+
+var Vwords = ["valley", "value", "various", "verb", "very", "view", "village", "visit", "voice", "vowel"];
+
+var Wwords = ["wait", "walk", "wall", "want", "war", "warm", "was", "wash", "Washington", "wasn’t", "watch", "water", "waves", "way", "we", "wear", "weather", "week", "weight", "well", "went", "were", "west", "western", "we’ll", "what", "wheels", "when", "where", "whether", "which", "while", "white", "who", "whole", "whose", "why", "wide", "wife", "wild", "will", "win", "wind", "window", "wings", "winter", "wire", "wish", "with", "within", "without", "woman", "women", "wonder", "won’t", "wood", "words", "work", "workers", "world", "would", "wouldn’t", "write", "written", "wrong", "wrote"];
+
+var Ywords = ["yard", "years", "yellow", "yes", "yet", "you", "young", "your", "yourself", "you’re"];
+
+var Computerphrases = ["That's great to hear! How did you find out?", "Send nudes? ;)", "Fun! Are you going to the park after work?", "You’re being ridiculous", "Please don't use that tone with me.", "What?", "I forgot to tell you how nice you looked today!", "You're making me uncomfortable.", "Can you not?", "Ha!", "I think there's an emojii for that.", "You up?", "Did you mean to send that to me?", "You are soooooooooooooooooooooooooooooooooooooooooooooooooooooo right!", "You know what? That actually sounds so familiar.  Did Brenda tell you that? She must have! Or did I hear it from someone who heard it from her? Anyhoo, it's disgusting and I wish I never knew. I just can't believe they would do that in public, epecially with children around!", "I KNEW I shouldn't have text you.", "Send nudes? ;)", "*peach emoji*"];
+
+var messageCount = 0;
+
+$("button").click(function() {
+	var leftPos = $('.sms-wrapper').scrollLeft();
+  		$(".sms-wrapper").animate({scrollLeft: leftPos + 900}, 900);
+	var userString = $(".myUserString").val();
+	console.log(userString);
+	var userWords = userString.split(" ");
+	console.log(userWords);
+	var generated_phrase = '';
+
+	// $(".sms-wrapper").append("<div class='box response-" + messageCount + "'>" + userString + "</div>");
+	$('.myUserString').val('');
+
+	for (var i=0; i < userWords.length; i++) {
+		var firstLetter = userWords[i].charAt(0);
+
+		if (firstLetter == "a") {
+			generated_phrase += Awords[Math.floor(Math.random()*Awords.length)] + ' ';
+		} else if (firstLetter == "b") {
+			generated_phrase += Bwords[Math.floor(Math.random()*Bwords.length)] + ' ';
+		} else if (firstLetter == "c") {
+			generated_phrase += Cwords[Math.floor(Math.random()*Cwords.length)] + ' ';
+		} else if (firstLetter == "d") {
+			generated_phrase += Dwords[Math.floor(Math.random()*Dwords.length)] + ' ';
+		} else if (firstLetter == "e") {
+			generated_phrase += Ewords[Math.floor(Math.random()*Ewords.length)] + ' ';
+		} else if (firstLetter == "f") {
+			generated_phrase += Fwords[Math.floor(Math.random()*Fwords.length)] + ' ';
+		} else if (firstLetter == "g") {
+			generated_phrase += Gwords[Math.floor(Math.random()*Gwords.length)] + ' ';
+		} else if (firstLetter == "h") {
+			generated_phrase += Hwords[Math.floor(Math.random()*Hwords.length)] + ' ';
+		} else if (firstLetter == "i") {
+			generated_phrase += Iwords[Math.floor(Math.random()*Iwords.length)] + ' ';
+		} else if (firstLetter == "j") {
+			generated_phrase += Jwords[Math.floor(Math.random()*Jwords.length)] + ' ';
+		} else if (firstLetter == "k") {
+			generated_phrase += Kwords[Math.floor(Math.random()*Kwords.length)] + ' ';
+		} else if (firstLetter == "l") {
+			generated_phrase += Lwords[Math.floor(Math.random()*Lwords.length)] + ' ';
+		} else if (firstLetter == "m") {
+			generated_phrase += Mwords[Math.floor(Math.random()*Mwords.length)] + ' ';
+		} else if (firstLetter == "n") {
+			generated_phrase += Nwords[Math.floor(Math.random()*Nwords.length)] + ' ';
+		} else if (firstLetter == "o") {
+			generated_phrase += Owords[Math.floor(Math.random()*Owords.length)] + ' ';
+		} else if (firstLetter == "p") {
+			generated_phrase += Pwords[Math.floor(Math.random()*Pwords.length)] + ' ';
+		} else if (firstLetter == "q") {
+			generated_phrase += Qwords[Math.floor(Math.random()*Qwords.length)] + ' ';
+		} else if (firstLetter == "r") {
+			generated_phrase += Rwords[Math.floor(Math.random()*Rwords.length)] + ' ';
+		} else if (firstLetter == "s") {
+			generated_phrase += Swords[Math.floor(Math.random()*Swords.length)] + ' ';
+		} else if (firstLetter == "t") {
+			generated_phrase += Twords[Math.floor(Math.random()*Twords.length)] + ' ';
+		} else if (firstLetter == "u") {
+			generated_phrase += Uwords[Math.floor(Math.random()*Uwords.length)] + ' ';
+		} else if (firstLetter == "v") {
+			generated_phrase += Vwords[Math.floor(Math.random()*Vwords.length)] + ' ';
+		} else if (firstLetter == "w") {
+			generated_phrase += Wwords[Math.floor(Math.random()*Wwords.length)] + ' ';
+		} else if (firstLetter == "y") {
+			generated_phrase += Ywords[Math.floor(Math.random()*Ywords.length)] + ' ';
+		}
+
+	}
+
+		$(".sms-wrapper").append("<div class='box2 response-" + messageCount + "'>" + generated_phrase + "</div>");
+		$(".sms-wrapper").append("<div class='box1 computer-sms-" + messageCount + "'>" + Computerphrases[Math.floor(Math.random()*Computerphrases.length)] + "</div>");
+
+});
+
+// $(".computer-sms").###
+
+
